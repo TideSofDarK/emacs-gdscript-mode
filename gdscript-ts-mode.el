2bc271efa6b9d832ab19ca3d83d67dc7a98e13e1
TideS <tidesmain@gmail.com>
2025-04-10 16:43:29 +0500

    More treesit rules

--- gdscript-ts-mode.el
+++ gdscript-ts-mode.el
@@ -116,7 +116,7 @@ It must be a function with two arguments: TYPE and NAME.")
 
    :language 'gdscript
    :feature 'type
-   `(["int" "bool" "float"] @font-lock-type-face
+   `((["int" "bool" "float"] @font-lock-type-face)
      (enum_definition name: (_) @font-lock-type-face)
      ((identifier) @font-lock-type-face
       (:match ,gdscript-ts--type-regex @font-lock-type-face))
