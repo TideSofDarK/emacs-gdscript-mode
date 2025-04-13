21e47cf5238d4d5c121216f016c5dc135f8c7613
TideS <tidesmain@gmail.com>
2025-04-13 20:30:47 +0500

    Types

--- gdscript-ts-mode.el
+++ gdscript-ts-mode.el
@@ -147,7 +147,8 @@ It must be a function with two arguments: TYPE and NAME.")
 
    :language 'gdscript
    :feature 'keyword
-   `((_ _ @font-lock-keyword-face (:match ,gdscript-ts--keyword-regex @font-lock-keyword-face)))
+   `((_ @font-lock-keyword-face (:match ,gdscript-ts--keyword-regex @font-lock-keyword-face))
+     (_ _ @font-lock-keyword-face (:match ,gdscript-ts--keyword-regex @font-lock-keyword-face)))
 
    :language 'gdscript
    :feature 'string
